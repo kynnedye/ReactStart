39ef9cc709333855311a392124b082c7a31c1336
kynnedye <90293916+kynnedye@users.noreply.github.com>
2022-01-15 19:01:02 -0500

    adding files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+import "App.css"
+
+
+export default function App(){
+    return(
+        
+        <h1>Hello world!!</h1>
+      
+        
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+//Import App
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
